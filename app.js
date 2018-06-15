adabc23a625b8d5d8b879f0ac2caa5e6824d08fc
Celine Gauchey <celinegauchey@gmail.com>
2018-06-15 12:17:11 -0400

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const morgan = require('morgan')
 const express = require("express")
 const bodyParser = require('body-parser')
 const app = express()
+const layout = require("./views/layout")
+const { db } = require('./models');
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( {extended: true} ))
@@ -9,11 +12,21 @@ app.use(morgan("dev"))
 app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
-  res.send('hello world')
+  res.send(layout('hello world'))
 })
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
+
+const init = async() => {
+  await db.sync({force: true})
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+
+init();
